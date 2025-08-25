e88c638ed0bdba09593b3e3b41fa9466dbc11e7e
HigorMenzinger <hmenzinger1997@outlook.com>
2025-08-24 21:28:42 -0400

    task list

---
+++ script.js
@@ -0,0 +1,51 @@
+const inputTask = document.getElementById("input-task");
+const sendBtn = document.getElementById("send-btn");
+const taskList = document.querySelector("#list ul");
+
+let tasks = [];
+
+function renderTaks() {
+  taskList.innerHTML = "";
+  tasks.forEach((todo) => {
+    const liElement = document.createElement("li");
+    const taskElement = document.createTextNode(todo);
+
+    let buttonElement = document.createElement("button");
+    let btnText = document.createTextNode("Excluir");
+
+    buttonElement.appendChild(btnText);
+
+    const position = tasks.indexOf(todo);
+
+    buttonElement.setAttribute("onclick", `removeTask(${position})`);
+
+    liElement.appendChild(taskElement);
+    liElement.appendChild(buttonElement);
+    taskList.appendChild(liElement);
+  });
+}
+
+function addTask() {
+  if (inputTask.value === "") {
+    alert("digite uma tarefa");
+    return false;
+  } else {
+    const newTask = inputTask.value;
+    tasks.push(newTask);
+    inputTask.value = "";
+    renderTaks();
+    saveLocalStorage();
+  }
+}
+
+function saveLocalStorage() {
+  localStorage.setItem("@taskList", JSON.stringify(tasks));
+}
+
+function removeTask(position) {
+  tasks.splice(position, 1);
+  renderTaks();
+  saveLocalStorage();
+}
+
+sendBtn.onclick = addTask;
